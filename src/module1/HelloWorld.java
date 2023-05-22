86392eae36110b65a86d6661139b6a4e4513576f
shubham <shubhamverma1091@gmail.com>
2023-05-22 22:32:42 +0530

    getting started

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -66,14 +66,15 @@ public class HelloWorld extends PApplet
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
 		map1 = new UnfoldingMap(this, 20, 50, 350, 500, provider);
-
+		map2 = new UnfoldingMap(this, 430, 50, 350, 500, provider);
 
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
 	    map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
-
+	    map2.zoomAndPanTo(zoomLevel, new Location(-22.9f, -43.2f));
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
+		MapUtils.createDefaultEventDispatcher(this, map2);
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
 
@@ -84,6 +85,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
